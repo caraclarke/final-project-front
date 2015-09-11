e1318b9d6163650f7e4ae3f9a4fc6bd837476039
caraclarke <cara.clarke8@gmail.com>
2015-09-11 10:35:55 -0400

    Switched from localhost to heroku

--- js/services/userFactory.js
+++ js/services/userFactory.js
@@ -3,25 +3,26 @@
     // create address factory
     var UserFactory = function($http) {
       var userAPI = {};
+			var sa = 'https://still-dawn-2794.herokuapp.com';
 
 			userAPI.login = function(email, password) {
-				return $http.post('http://localhost:3000/login/', { 'credentials': { email: email, password: password }});
+				return $http.post(sa + '/login/', { 'credentials': { email: email, password: password }});
 			};
 
 			userAPI.getUser = function(userId) {
-        return $http.get('http://localhost:3000/users/' + userId, {headers: { Authorization: 'Token token=' + simpleStorage.get('token')}});
+        return $http.get(sa + '/users/' + userId, {headers: { Authorization: 'Token token=' + simpleStorage.get('token')}});
       };
 
       userAPI.create = function(user) {
-        return $http.post('http://localhost:3000/users/', { user: user });
+        return $http.post(sa + '/users/', { user: user });
       };
 
       userAPI.update = function(user) {
-        return $http.patch('http://localhost:3000/users/' + userId, user, {headers: { Authorization: 'Token token=' + simpleStorage.get('token')}});
+        return $http.patch(sa + '/users/' + userId, user, {headers: { Authorization: 'Token token=' + simpleStorage.get('token')}});
       };
 
       userAPI.delete = function(user) {
-        return $http.delete('http://localhost:3000/users/' + userId, {headers: { Authorization: 'Token token=' + simpleStorage.get('token')}});
+        return $http.delete(sa + '/users/' + userId, {headers: { Authorization: 'Token token=' + simpleStorage.get('token')}});
       };
       return userAPI;
     }; // end userFactory
